68d45d0ef801ffc58bdaaf28274278e7cfd6d5e1
Shubh Arya <shubharya001@gmail.com>
2025-10-07 05:11:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Smart-Health-disease-prediction-system-2.0/',
+  // Fix for "Blocked request" error on Render
+  preview: {
+    allowedHosts: [
+      'smart-health-disease-prediction-system.onrender.com', 
+    ],
+  },
 })
