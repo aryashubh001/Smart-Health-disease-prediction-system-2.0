e9eda0c6b748078489a0d6474dbbb5cdde90259f
Shubh Arya <shubharya001@gmail.com>
2025-10-07 01:08:15 +0530

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Smart-Health-disease-prediction-system-2.0/',
+})
